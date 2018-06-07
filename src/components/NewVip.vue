2090796586906546a3ba17513d6d2da21732ce1d
cwj <1755140651@qq.com>
2018-06-07 17:28:07 +0800

    会员管理

--- src/components/NewVip.vue
+++ src/components/NewVip.vue
@@ -1,8 +1,9 @@
+<!--会员管理-->
 <template>
   <el-row class="warp">
     <el-col :span="24" class="warp-breadcrum">
       <el-breadcrumb separator="/">
-        <el-breadcrumb-item :to="{ path: '/' }"><b>管理员管理</b></el-breadcrumb-item>
+        <el-breadcrumb-item :to="{ path: '/' }"><b>会员管理</b></el-breadcrumb-item>
       </el-breadcrumb>
     </el-col>
 
@@ -11,7 +12,7 @@
       <el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
         <el-form :inline="true" :model="filters">
           <el-form-item>
-            <el-input v-model="filters.name" placeholder="会员名" @keyup.enter.native="handleSearch"></el-input>
+            <el-input v-model="filters.nickname" placeholder="会员名" @keyup.enter.native="handleSearch"></el-input>
           </el-form-item>
           <el-form-item>
             <el-button type="primary" v-on:click="handleSearch">查询</el-button>
@@ -22,78 +23,137 @@
       <!-- 会员列表-->
       <el-table :data="vip" highlight-current-row @selection-change="selsChange"
                 style="width: 100%;">
-        <el-table-column type="selection" width="55"></el-table-column>
         <el-table-column type="index" width="60"></el-table-column>
-        <el-table-column prop="nickname" label="用户名" width="100" sortable></el-table-column>
-        <el-table-column prop="user" label="姓名" width="100" sortable></el-table-column>
-        <el-table-column prop="sex" label="性别" width="100" sortable></el-table-column>
-        <el-table-column prop="idCard" label="身份证号" width="160" sortable></el-table-column>
-        <el-table-column prop="createTime" label="添加时间" width="160" sortable>
+        <el-table-column prop="headimgurl" label="会员头像" width="100" sortable>
+          <template slot-scope="scope">
+            <img :src="scope.row.headimgurl" alt="" style="width: 50px;height: 50px">
+          </template>
+        </el-table-column>
+        <el-table-column prop="nickname" label="会员名称" width="100" sortable></el-table-column>
+        <el-table-column prop="gender" label="会员性别" width="100" sortable></el-table-column>
+        <el-table-column prop="phone" label="手机号" width="140" sortable></el-table-column>
+        <el-table-column prop="createTime" label="注册时间" width="180" sortable>
           <template slot-scope="scope">
             {{ scope.row.createTime | dateTimeFormat }}
           </template>
         </el-table-column>
+        <el-table-column prop="vipEndTime" label="会员到期时间" width="180" sortable>
+          <template slot-scope="scope">
+            {{ scope.row.vipEndTime | dateTimeFormat }}
+          </template>
+        </el-table-column>
       </el-table>
+
+      <!--工具条-->
+      <el-col :span="24" class="toolbar">
+        <el-pagination layout="prev, pager, next" @current-change="handleCurrentChange" :page-size="15" :total="total"
+                       style="float:right;">
+        </el-pagination>
+      </el-col>
 
     </el-col>
   </el-row>
 </template>
 
 <script>
-    export default {
-        name: "NewVip",
 
-      data() {
-        return {
-          filters: {
-            name: ''
-          },
-          books: [],
-          total: 0,
-          page: 1,
-          limit: 10,
-          loading: false,
-          sels: [], //列表选中列
-          editForm: {
-            id: 0,
-            name: '',
-            author: '',
-            publishAt: '',
-            description: ''
-          },
-          addForm: {
-            name: '',
-            author: '',
-            publishAt: '',
-            description: ''
-          }
-        }
-      },
-      methods: {
-        handleCurrentChange(val) {
-          this.page = val;
-          this.search();
+  import axios from 'axios'
+
+  export default {
+    name: "NewVip",
+    data() {
+      return {
+        vip: [],
+        filters: {
+          name: ''
         },
-        handleSearch() {
-          this.total = 0;
-          this.page = 1;
-          this.search();
-        },
-        search() {
-          let that = this;
-          that.loading = false;
-        },
-      },
-      showAddDialog: function () {
-        this.addFormVisible = true;
-        this.addForm = {
-          name: '',
-          author: '',
-          publishAt: '',
-          description: ''
-        };
+        total: 0,
+        limit: 10,
+        loading: false,
       }
+    },
+    methods: {
+      handleCurrentChange(val) {
+        this.page = val;
+        this.handleSearch(this.page);
+      },
+      dateTimeFormat(value) {
+        let time = new Date(+value);
+        let rightTwo = (v) => {
+          v = '0' + v;
+          return v.substring(v.length - 2, v.length)
+        };
+        if (time == null) return;
+        let year = time.getFullYear();
+        let month = time.getMonth() + 1;
+        let date = time.getDate();
+        let hours = time.getHours();
+        let minutes = time.getMinutes();
+        let seconds = time.getSeconds();
+        return year + '-' + rightTwo(month) + '-' + rightTwo(date) + ' ' + rightTwo(hours) + ':' + rightTwo(minutes) + ':' + rightTwo(seconds);
+      },
+      handleSearch() {
+        this.loading = true;//显示加载条
+        axios({//根据会员昵称查询
+          method: 'post',
+          url: '/api/memberCtrl/queryMember',
+          headers: {
+            'Content-type': 'application/x-www-form-urlencoded'
+          },
+          params: {
+            'size': '15',//每页数量
+            'page': this.page,//当前页
+            'nickname': this.filters.nickname
+          }
+        })
+          .then((res) => {
+              this.loading = false;//隐藏加载条
+              this.vip = res.data.memberList;
+              this.total = res.data.pageNumber;//总页数
+              for (let i = 0; i < this.vip.length; i++) {
+                this.vip[i].vipEndTime = this.dateTimeFormat(this.vip[i].vipEndTime);
+                this.vip[i].createTime = this.dateTimeFormat(this.vip[i].createTime);
+              }
+            },
+          ).catch((e) => {
+          if (e && e.response) {
+            switch (e.response.status) {
+              case 504:
+                this.loading = true;//显示加载条
+                this.$message({
+                  showClose: true,
+                  message: '服务器异常',
+                  type: 'warning'
+                });
+                break
+              case 500:
+                this.loading = true;//显示加载条
+                this.$message({
+                  showClose: true,
+                  message: '服务器异常',
+                  type: 'warning'
+                });
+                break
+              case 405:
+                this.loading = true;//显示加载条
+                this.$message({
+                  showClose: true,
+                  message: '请先登录',
+                  type: 'warning'
+                });
+                break
+            }
+          }
+        });
+      },
+      selsChange: function (sels) {
+        this.sels = sels;
+      },
+    },
+    mounted() { //初始化页面
+      this.handleSearch()
     }
+  }
 </script>
 
 <style>
