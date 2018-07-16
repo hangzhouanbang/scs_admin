6bf2b1fd283a2961f82d230ef3e5e1717e8b266a
cwj <1755140651@qq.com>
2018-07-16 18:08:08 +0800

    会员卡购买、消费记录

--- src/components/Activity.vue
+++ src/components/Activity.vue
@@ -1,13 +1,307 @@
+<!--活动管理-->
 <template>
+  <el-row class="warp">
+    <el-col :span="24" class="warp-breadcrum">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item :to="{ path: '/' }"><b>活动管理</b></el-breadcrumb-item>
+      </el-breadcrumb>
+    </el-col>
 
+    <el-col :span="24" class="warp-main" v-loading="loading" element-loading-text="拼命加载中">
+      <!--工具条-->
+      <el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
+        <el-form :inline="true" :model="filters">
+          <el-form-item>
+            <el-button type="primary" @click="showAddDialog">新建活动</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+
+      <!-- 系统维护列表-->
+      <el-table :data="list" highlight-current-row @selection-change="selsChange"
+                style="width: 100%;">
+        <el-table-column prop="title" label="活动标题" width="100" sortable></el-table-column>
+        <el-table-column prop="file" label="内容" width="100" sortable>
+          <template slot-scope="scope">
+            <img :src="scope.row.file" alt="" style="width: 50px;height: 50px">
+          </template>
+        </el-table-column>
+        <el-table-column prop="number" label="活动地址" width="100" sortable></el-table-column>
+        <el-table-column prop="remainSecond" label="操作">
+          <template slot-scope="scope">
+            <el-button size="small" @click="publishDialog(scope.$index,scope.row)">停用</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <!--工具条-->
+      <el-col :span="24" class="toolbar">
+        <el-pagination layout="prev, pager, next" @current-change="handleCurrentChange" :page-size="1" :total="total"
+                       style="float:right;">
+        </el-pagination>
+      </el-col>
+
+      <!--新建活动弹窗-->
+      <el-dialog title="系统维护" :visible.sync="addFormVisible" :close-on-click-modal="false">
+        <el-form :model="normalForm" label-width="100px" :rules="rules" class="demo-ruleForm">
+          <el-form-item label="标题" prop="title">
+            <el-input v-model="normalForm.title" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="图片" prop="file">
+            <div class="upload">
+              <el-upload
+                class="avatar-uploader"
+                :action=domain
+                :http-request=upqiniu
+                :show-file-list="false"
+                :before-upload="beforeUpload">
+                <img v-if="imageUrl" :src="imageUrl" class="avatar">
+                <el-button v-else class="el-icon-plus avatar-uploader-icon"></el-button>
+              </el-upload>
+            </div>
+          </el-form-item>
+          <el-form-item label="活动地址" prop="address">
+            <el-input v-model="normalForm.address" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="issue">发布</el-button>
+            <el-button type="primary" @click.native="addFormVisible = false">取消</el-button>
+          </el-form-item>
+        </el-form>
+      </el-dialog>
+
+    </el-col>
+  </el-row>
 </template>
 
 <script>
-    export default {
-        name: "Activity"
+  import axios from 'axios'
+
+  export default {
+    name: "Activity",
+
+    data() {
+      return {
+        imageUrl: '',
+        // 七牛云的上传地址，根据自己所在地区选择，这里是华东区
+        domain: 'http://up.qiniu.com',
+        // 这是七牛云空间的外链默认域名
+        qiniuaddr: 'paly4iump.bkt.clouddn.com',
+        normalForm: {},
+        rules: {
+          title: [
+            {required: true, message: '请输入标题', trigger: 'blur'}
+          ],
+          file: [
+            {required: true, message: '请选择图片', trigger: 'blur'}
+          ],
+          address: [
+            {required: true, message: '请选择图片', trigger: 'blur'}
+          ]
+        },
+        list: [],
+        filters: {
+          name: ''
+        },
+        total: 0,
+        loading: false,
+        addFormVisible: false,//系统维护页面是否显示
+      }
+    },
+    methods: {
+      // 上传文件到七牛云
+      upqiniu(req) {
+        //console.log(req)
+        const config = {
+          headers: {'Content-Type': 'multipart/form-data'}
+        }
+        let filetype = ''
+        if (req.file.type === 'image/png') {
+          filetype = 'png'
+        } else {
+          filetype = 'jpg'
+        }
+        // 重命名要上传的文件
+        const keyname = 'anbang' + Math.random() + '.' + filetype
+        // 从后端获取上传凭证token
+        axios({
+          method: 'post',
+          url: '/api/mailctrl/uptoken',
+          headers: {
+            'Content-type': 'application/x-www-form-urlencoded'
+          },
+          params: {
+            'accessKey': 'qQj7mRKyvE7dOOjObMC8W58i6Yn3penfr7-_fg4d',
+            'secretKey': '9f70kmAddF1maP1U0jy0vRNAhwWNv_huR1xDSH_s',
+            'bucket': 'anbang'
+          }
+        }).then(res => {
+          const formdata = new FormData()
+          formdata.append('file', req.file)
+          formdata.append('token', res.data.data)
+          formdata.append('key', keyname)
+          // 获取到凭证之后再将文件上传到七牛云空间
+          axios.post(this.domain, formdata, config).then(res => {
+            this.imageUrl = 'http://' + this.qiniuaddr + '/' + res.data.key
+            //console.log(this.imageUrl)
+          })
+        })
+      },
+      // 验证文件合法性
+      beforeUpload(file) {
+        const isJPG = file.type === 'image/jpeg' || file.type === 'image/png'
+        const isLt2M = file.size / 1024 / 1024 < 2
+        if (!isJPG) {
+          this.$message.error('上传头像图片只能是 JPG 格式!')
+        }
+        if (!isLt2M) {
+          this.$message.error('上传头像图片大小不能超过 2MB!')
+        }
+        return isJPG && isLt2M
+      },
+
+      //发布
+      issue(req) {
+        if (this.normalForm.title == undefined || this.normalForm.title == "") {
+          this.$message({
+            showClose: true,
+            message: '标题和图片不能为空',
+            type: 'warning'
+          });
+        } else {
+          axios({
+            method: 'post',
+            url: '/api/',
+            headers: {
+              'Content-type': 'application/x-www-form-urlencoded'
+            },
+            params: {
+              'status': '0',
+              'title': this.normalForm.title,
+              'file': this.imageUrl
+            }
+          })
+            .then((res) => {
+                this.$message({
+                  showClose: true,
+                  message: '发布成功',
+                  type: 'success'
+                });
+                this.normalForm.title = ''
+                this.normalForm.file = ''
+                this.addFormVisible = false;//关闭弹窗
+                this.handleSearch();
+              },
+            ).catch((e) => {
+            if (e && e.response) {
+              switch (e.response.status) {
+                case 504:
+                  this.$message({
+                    showClose: true,
+                    message: '服务器异常',
+                    type: 'warning'
+                  });
+                  this.loading = false;//隐藏加载条
+                  break
+                case 500:
+                  this.$message({
+                    showClose: true,
+                    message: '服务器异常',
+                    type: 'warning'
+                  });
+                  this.loading = false;//隐藏加载条
+                  break
+                case 405:
+                  this.$message({
+                    showClose: true,
+                    message: '请先登录',
+                    type: 'warning'
+                  });
+                  break
+              }
+            }
+          });
+        }
+      },
+      showAddDialog: function () {
+        this.addFormVisible = true;
+        this.addForm = {
+          name: '',
+          author: '',
+          publishAt: '',
+          description: ''
+        };
+      },
+      handleCurrentChange(val) {
+        this.page = val;
+        this.handleSearch(this.page);
+      },
+      handleSearch() {
+        axios({
+          method: 'post',
+          url: '/api/',
+          headers: {
+            'Content-type': 'application/x-www-form-urlencoded'
+          },
+          params: {
+            'status': '0',
+            'size': '15',//每页数量
+            'page': this.page,//当前页
+            'adminname': this.filters.adminname
+          }
+        })
+          .then((res) => {
+              this.loading = false;//隐藏加载条
+              this.list = res.data.list;
+              this.total = res.data.count;//总页数
+            },
+          ).catch((e) => {
+          if (e && e.response) {
+            switch (e.response.status) {
+              case 504:
+                this.$message({
+                  showClose: true,
+                  message: '服务器异常',
+                  type: 'warning'
+                });
+                this.loading = false;//隐藏加载条
+                break
+              case 500:
+                this.$message({
+                  showClose: true,
+                  message: '服务器异常',
+                  type: 'warning'
+                });
+                this.loading = false;//隐藏加载条
+                break
+              case 405:
+                this.$message({
+                  showClose: true,
+                  message: '请先登录',
+                  type: 'warning'
+                });
+                break
+            }
+          }
+        });
+      },
+      selsChange: function (sels) {
+        this.sels = sels;
+      },
+    },
+    mounted() { //初始化页面
+      this.handleSearch()
     }
+  }
 </script>
 
 <style scoped>
+  .warp-main {
+    margin-top: 20px;
+  }
 
+  img {
+    width: 180px;
+    height: 120px;
+  }
 </style>
--- src/components/MembershipCardPurchaseRecord.vue
+++ src/components/MembershipCardPurchaseRecord.vue
@@ -1,13 +1,146 @@
+<!--会员卡购买记录-->
 <template>
+  <el-row class="warp">
+    <el-col :span="24" class="warp-breadcrum">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item :to="{ path: '/' }"><b>推广员中心</b></el-breadcrumb-item>
+        <el-breadcrumb-item>会员卡购买记录</el-breadcrumb-item>
+      </el-breadcrumb>
+    </el-col>
 
+    <el-col :span="24" class="warp-main" v-loading="loading" element-loading-text="拼命加载中">
+      <!--工具条-->
+      <el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
+        <el-form :inline="true" :model="filters">
+          <el-form-item label="推广员ID">
+            <el-input v-model="filters.game" placeholder="单行输入"></el-input>
+          </el-form-item>
+          <el-form-item label="注册时间">
+            <el-date-picker
+              v-model="value1"
+              type="date"
+              placeholder="时间选择">
+            </el-date-picker>
+          </el-form-item>
+          <el-form-item label="至">
+            <el-date-picker
+              v-model="value2"
+              type="date"
+              placeholder="时间选择">
+            </el-date-picker>
+          </el-form-item>
+          <el-button type="primary" @click="seek">查询</el-button>
+        </el-form>
+      </el-col>
+
+      <!-- 会员卡购买记录列表-->
+      <el-table :data="items" highlight-current-row @selection-change="selsChange"
+                style="width: 100%;">
+        <el-table-column prop="date" label="昵称" width="100" sortable></el-table-column>
+        <el-table-column prop="newMember" label="ID" width="100" sortable></el-table-column>
+        <el-table-column prop="currentMember" label="商品名称" width="100" sortable></el-table-column>
+        <el-table-column prop="cost" label="数量" width="100" sortable></el-table-column>
+        <el-table-column prop="gameNum" label="说明" width="120" sortable></el-table-column>
+        <el-table-column prop="loginMember" label="购买时间" width="100" sortable></el-table-column>
+        <el-table-column prop="remainSecond" label="累积购买金额"></el-table-column>
+      </el-table>
+      <!--工具条-->
+      <el-col :span="24" class="toolbar">
+        <el-pagination layout="prev, pager, next" @current-change="handleCurrentChange" :page-size="1" :total="total"
+                       style="float:right;">
+        </el-pagination>
+      </el-col>
+
+    </el-col>
+  </el-row>
 </template>
 
 <script>
-    export default {
-        name: "MembershipCardPurchaseRecord"
+  import axios from 'axios'
+
+  export default {
+    name: "MembershipCardPurchaseRecord",
+    data() {
+      return {
+        loading: false,//隐藏加载条
+        filters: {
+          name: ''
+        },
+        items: [],
+        total: 0,
+        value1: '',//开始时间
+        value2: '',//结束时间
+      }
+    },
+    methods: {
+      handleCurrentChange(val) {
+        this.page = val;
+        this.seek(this.page);
+      },
+      //搜索
+      seek() {
+        axios({
+          method: 'post',
+          url: '/api/',
+          headers: {
+            'Content-type': 'application/x-www-form-urlencoded'
+          },
+          params: {
+            'size': '15',//每页数量
+            'page': this.page,//当前页
+            'startTime': new Date(this.value1).getTime(), /*日期转换为时间戳（毫秒数）发送到后台*/
+            'endTime': new Date(this.value2).getTime()
+          }
+        })
+          .then((res) => {
+              this.po = true;//显示表单
+              this.loading = false;//隐藏加载条
+              this.items = res.data.data.items;
+              this.total = res.data.data.pageCount;
+              //console.log(res.data.data.items)
+              for (let i = 0; i < this.items.length; i++) {
+                this.items[i].date = this.dateTimeFormat(this.items[i].date);
+              }
+            },
+          ).catch((e) => {
+          if (e && e.response) {
+            switch (e.response.status) {
+              case 504:
+                this.$message({
+                  showClose: true,
+                  message: '服务器异常',
+                  type: 'warning'
+                });
+                this.loading = false;//隐藏加载条
+                break
+              case 500:
+                this.$message({
+                  showClose: true,
+                  message: '服务器异常',
+                  type: 'warning'
+                });
+                this.loading = false;//隐藏加载条
+                break
+              case 405:
+                this.$message({
+                  showClose: true,
+                  message: '请先登录',
+                  type: 'warning'
+                });
+                break
+            }
+          }
+        });
+      },
+      selsChange: function (sels) {
+        this.sels = sels;
+      },
     }
+  }
 </script>
 
 <style scoped>
-
+  .warp-main {
+    margin-top: 30px;
+  }
 </style>
